133304a99414d81dcba46485293895d9436abe0d
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-22 22:56:35 -0800

    Add running detection to instance kill

--- src/event-map.ts
+++ src/event-map.ts
@@ -1,13 +1,14 @@
 import kill from 'tree-kill';
 
 import {TestInstance} from "../types/pure";
+import isRunning from 'is-running';
 
 const eventMap = {
   down: (instance: TestInstance) => instance.stdin.write('\x1B\x5B\x42'),
   up: (instance: TestInstance) => instance.stdin.write('\x1B\x5B\x41'),
   enter: (instance: TestInstance) => instance.stdin.write('\x0D'),
-  sigterm: (instance: TestInstance) => instance.pid && kill(instance.pid),
-  sigkill: (instance: TestInstance) => instance.pid && kill(instance.pid, 'SIGKILL'),
+  sigterm: (instance: TestInstance) => instance.pid && isRunning(instance.pid) && kill(instance.pid),
+  sigkill: (instance: TestInstance) => instance.pid && isRunning(instance.pid) && kill(instance.pid, 'SIGKILL'),
   type: (instance: TestInstance, text: string) => instance.stdin.write(text)
 }
 
