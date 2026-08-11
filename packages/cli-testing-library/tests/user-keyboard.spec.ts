b99a25780765ef891598dd5734d9da452f3ad5a2
Corbin Crutchley <git@crutchcorn.dev>
2026-08-10 22:00:19 -0700

    Update keymap (#51)
    * fix: handle forward/backslashes and ctrl
    
    * fix: fix '(' mapping add more keyboard mapping
    
    # Conflicts:
    #	packages/cli-testing-library/src/user-event/keyboard/keyMap.ts
    
    * ci: apply automated fixes and generate docs
    
    ---------
    
    Co-authored-by: Romuald Oluwatobi <romualdnoualinon@gmail.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/cli-testing-library/tests/user-keyboard.spec.ts
+++ packages/cli-testing-library/tests/user-keyboard.spec.ts
@@ -2,6 +2,7 @@ import { resolve } from "node:path";
 import { expect, test } from "vitest";
 import { render } from "../src/pure";
 import { fireEvent } from "../src/events";
+import { waitFor } from "../src/wait-for";
 
 test("Should render { and } in user keyboard", async () => {
   const { findByText, userEvent: userEventLocal } = await render("node", [
@@ -16,4 +17,30 @@ test("Should render { and } in user keyboard", async () => {
   expect(await findByText("{Hello}")).toBeTruthy();
 
   await fireEvent.sigterm(instance);
+});
+
+test("Should render forward and backslashes in user keyboard", async () => {
+  const { findByText, userEvent: userEventLocal } = await render("node", [
+    resolve(__dirname, "./execute-scripts/stdio-inquirer-input.js"),
+  ]);
+
+  const instance = await findByText("What is your name?");
+
+  userEventLocal.keyboard("/test-dir\\");
+
+  expect(await findByText("/test-dir\\")).toBeTruthy();
+
+  await fireEvent.sigterm(instance);
+});
+
+test("Should cancel a process with Ctrl", async () => {
+  const { findByText, userEvent: userEventLocal } = await render("node", [
+    resolve(__dirname, "./execute-scripts/stdio-inquirer-input.js"),
+  ]);
+
+  const instance = await findByText("What is your name?");
+
+  userEventLocal.keyboard("[Ctrl]c");
+
+  await waitFor(() => expect(instance.hasExit()).toBeTruthy());
 });
